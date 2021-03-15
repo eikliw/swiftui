695861f2f3d03cfc5b803393a81d7043ffb80792
Adam Wilkie <eikliw@gmail.com>
2021-03-15 16:06:23 -0700

    Create piggybank.swift

---
+++ piggybank.swift
@@ -0,0 +1,14 @@
+var pesos: Double = 6043.0
+var reais: Double = 18500.0
+var soles: Double = 9345.0
+var total: Double
+
+total = 0.048 * pesos + 0.18 * reais + 0.27 * soles
+
+/*
+1 Peso equals 0.048 USD
+1 Real equals 0.18 USD
+1 Sol squals 0.27 USD
+*/
+
+print("US Dollars = $\(total)")
