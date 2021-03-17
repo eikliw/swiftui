8a36b08891b2a7446269ddea3d21fa361a0904ff
Adam Wilkie <eikliw@gmail.com>
2021-03-17 15:08:31 -0700

    Update villains.swift

--- villains.swift
+++ villains.swift
@@ -1,7 +1,7 @@
 var episode = 6
 var villain: String 
 
-// Write your code below 🦹
+// determine the villain based on episode number with switch interval matching
 switch episode {
   case 1...3:
     villain = "Emperor Palpatine"
