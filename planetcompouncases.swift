379ea6be2bada68bdf2139efb6808e6be7bea6b2
Adam Wilkie <eikliw@gmail.com>
2021-03-17 15:14:26 -0700

    Create planetcompouncases.swift

---
+++ planetcompouncases.swift
@@ -0,0 +1,12 @@
+var planet = "Venus" 
+
+// code
+
+switch planet {
+ case "Earth","Mercury","Venus", "Mars":
+ print("Terrestrial planet")
+ case "Saturn", "Jupiter", "Uranus", "Neptune":
+ print("Jovian planet")
+ default:
+ print("Unknown planet")
+}
