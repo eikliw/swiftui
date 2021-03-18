3786752168cee448062a34cb4efdf7e4673a9735
Adam Wilkie <eikliw@gmail.com>
2021-03-18 10:34:59 -0700

    Create randomlotto.swift

---
+++ randomlotto.swift
@@ -0,0 +1,11 @@
+var rand1 = Int.random(in: 1...70)
+var rand2 = Int.random(in: 1...70)
+var rand3 = Int.random(in: 1...70)
+var rand4 = Int.random(in: 1...70)
+var rand5 = Int.random(in: 1...70)
+
+var power = Int.random(in: 1...30)
+print("Your numbers are:")
+print(rand1,rand2,rand3,rand4,rand5)
+print("Your power number is:")
+print(power)
